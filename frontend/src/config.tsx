135a7b07d63920484d3026756056c39fd76a0127
André Krosby <andre.krosby92@icloud.com>
2021-04-29 14:00:09 +0200

    Changed base-url.

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -3,13 +3,13 @@ import Cookies from 'js-cookie'
 export const BASE_URL = (
   window.location.href.includes('localhost') ?
     'http://localhost:8000' :
-    'https://<APP>.herokuapp.com'
+    'https://ak-messenger.herokuapp.com'
 )
 
 export const SOCKET_BASE_PATH = (
   window.location.href.includes('localhost') ?
     'ws://localhost:8000' :
-    'wss://<APP>.herokuapp.com'
+    'wss://ak-messenger.herokuapp.com'
 )
 
 export const CREDENTIALS = (
